ae4e3885d9ffd08fc94f3b5f42c76604a33d7289
O'tkir <keldibekovotkir767@gmail.com>
2025-10-12 08:51:21 +0500

    starter

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Param, Req } from '@nestjs/common';
+import { Controller, Get, Post, Param, Req, UseGuards } from '@nestjs/common';
 import { LikeService } from './like.service';
+import { JwtAuthGuard } from 'src/guard/auth.guard';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post(':adId')
   async toggle(@Param('adId') adId: string, @Req() req) {
-    const userId = req.user.id; 
+    const userId = req.user.id;
     return this.likeService.toggle(userId, { adId });
   }
 
